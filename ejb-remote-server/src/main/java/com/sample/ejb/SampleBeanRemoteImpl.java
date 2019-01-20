4095bd5b27982c5c12e70e7f0c53b6e493ab5693
jose lozano <jose.lozano@smartsoft.com.co>
2019-01-20 16:11:56 -0500

    Ejb remoto

---
+++ ejb-remote-server/src/main/java/com/sample/ejb/SampleBeanRemoteImpl.java
@@ -0,0 +1,16 @@
+package com.sample.ejb;
+ 
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+ 
+@Stateless
+@Remote(SampleBeanRemote.class) 
+public class  SampleBeanRemoteImpl implements SampleBeanRemote  {
+ 
+    @Override
+    public String echo(String s) {
+        return "Hello (Test) : " + s;
+    }
+ 
+ 
+}
